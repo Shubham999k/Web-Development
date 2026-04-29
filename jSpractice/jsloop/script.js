344d712be2f1015d668d3c8d939ab8ab97a2de5f
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-04-29 19:31:37 +0530

    Refactor project structure: move HTML and JS files to jsloop directory and update script content

---
+++ jSpractice/jsloop/script.js
@@ -0,0 +1,27 @@
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i);
+
+// }
+// console.log("Table of 2 given below");
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i * 4);
+
+// }
+// console.log("Below is indexing")
+// let a = [24, 58, 33, 86, 45, 78, 25, 96, 46, 58, 23]
+// for (let i = 0; i <= 10; i++) {
+//     console.log(a[i]);
+
+// }
+console.log("Table of 2 given below by while");
+let i =1;
+while(i<=10){
+  console.log(i*2);
+  i++;   
+}
+console.log("Table of 2 given below by do while");
+let j =1;
+do{
+  console.log(j*2);
+  j++;   
+}while(j<=10)
