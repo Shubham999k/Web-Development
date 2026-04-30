04c1b4e8baaf91c23c4ce73d0578bb9b073d8a70
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-04-30 19:52:32 +0530

    Fix title formatting in index.html and update script.js comments for clarity

--- jSpractice/jsloop/script.js
+++ jSpractice/jsloop/script.js
@@ -13,15 +13,89 @@
 //     console.log(a[i]);
 
 // }
-console.log("Table of 2 given below by while");
-let i =1;
-while(i<=10){
-  console.log(i*2);
-  i++;   
+// console.log("Table of 2 given below by while");
+// let i =1;
+// while(i<=10){
+//   console.log(i*2);
+//   i++;   
+// }
+// console.log("Table of 2 given below by do while");
+// let j =1;
+// do{
+//   console.log(j*2);
+//   j++;   
+// }while(j<=10)
+// let n =5;
+// if(n>=0){
+//   console.log("Positive Integer");
+// }
+// else{
+//   console.log("Negative Integer");
+// }
+// let n =51;
+// if(n>=20){
+//   console.log("eligible to vote");
+// }
+// else{
+//   console.log("not eligible to vote");
+// }
+// let n =51;
+// if(n%2==0){
+//   console.log("even");
+// }
+// else{
+//   console.log("odd");
+// }
+// let n =99;
+// if(n>=90 && n<75 ){
+//   console.log("A");
+// }
+// if(n>=75 && n<50){
+//   console.log("B");
+// }
+// if(n>=50 && n<75){
+//   console.log("B");
+// }
+// else{
+//   console.log("Fail");
+// }
+// let usr = "abcde";
+// let pass = "12345";
+// usr === "abcde" && pass === "12345"
+//   ? console.log("Successful") : console.log("Not Valid");
+
+
+// let data;
+// let name = "Shubham"
+
+// if(name){
+//   data = name;
+// }
+// else{
+//   data = "N/A";
+// }
+
+// data = name || "N/A"
+// console.log(data);
+
+// below is jump case
+
+let choice = 3;
+switch (choice) {
+  case 1: {
+    console.log("Tea");
+    break
+  }
+  case 2: {
+    console.log("Coffee");
+    break
+  }
+  case 3: {
+    console.log("Lassi");
+    break
+  }
+  default: {
+    console.log("Paani");
+
+  }
 }
-console.log("Table of 2 given below by do while");
-let j =1;
-do{
-  console.log(j*2);
-  j++;   
-}while(j<=10)
