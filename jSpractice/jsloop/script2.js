3c26da163404a63457d5873361379246f649cd4e
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-05-01 19:56:32 +0530

    Fix formatting and add sum function in script2.js

--- jSpractice/jsloop/script2.js
+++ jSpractice/jsloop/script2.js
@@ -1,6 +1,10 @@
-let abc = function(){
+let abc = function () {
     console.log("Hello World");
-    
+
 }
 console.log(abc);
 abc();
+
+const addsum = (a, b) => a + b;
+
+console.log(addsum(5,6));
