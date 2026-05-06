ba1e16b98ee206972c47883df07175fd52325666
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-05-06 19:25:43 +0530

    recover web-d

---
+++ jSpractice/Assignment-js/script.js
@@ -0,0 +1,108 @@
+// for (var i = 0; i <= 10; i++) {
+//     console.log(i);
+
+// }
+// console.log("Table of 2 given below");
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i * 4);
+          
+// }
+// console.log("Below is indexing")
+// let a = [24, 58, 33, 86, 45, 78, 25, 96, 46, 58, 23]
+// for (let i = 0; i <= 10; i++) {
+//     console.log(a[i]);
+
+// }
+// console.log("Table of 2 given below by while");
+// let i =1;
+// while(i<=10){
+//   console.log(i*2);
+//   i++;   
+// }
+// console.log("Table of 2 given below by do while");
+// let j =1;
+// do{
+//   console.log(j*2);
+//   j++;   
+// }while(j<=10)
+// let n =5;
+// if(n>=0){
+//   console.log("Positive Integer");
+// }
+// else{
+//   console.log("Negative Integer");
+// }
+// let n =51;
+// if(n>=20){
+//   console.log("eligible to vote");
+// }
+// else{
+//   console.log("not eligible to vote");
+// }
+// let n =51;
+// if(n%2==0){
+//   console.log("even");
+// }
+// else{
+//   console.log("odd");
+// }
+// let n =99;
+// if(n>=90 && n<75 ){
+//   console.log("A");
+// }
+// if(n>=75 && n<50){
+//   console.log("B");
+// }
+// if(n>=50 && n<75){
+//   console.log("B");
+// }
+// else{
+//   console.log("Fail");
+// }
+// let usr = "abcde";
+// let pass = "12345";
+// usr === "abcde" && pass === "12345"
+//   ? console.log("Successful") : console.log("Not Valid");
+
+
+// let data;
+// let name = "Shubham"
+
+// if(name){
+//   data = name;
+// }
+// else{
+//   data = "N/A";
+// }
+
+// data = name || "N/A"
+// console.log(data);
+
+// below is jump case
+
+// let choice = 3;
+// switch (choice) {
+//   case 1: {
+//     console.log("Tea");
+//     break
+//   }
+//   case 2: {
+//     console.log("Coffee");
+//     break
+//   }
+//   case 3: {
+//     console.log("Lassi");
+//     break
+//   }
+//   default: {
+//     console.log("Paani");
+
+//   }
+// }
+let a = 80, b = 20;
+function sum(x, y) {
+  z = x + y;
+  console.log(z);
+
+}
+sum(a, b);
