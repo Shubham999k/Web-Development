83b4ac47260799e83204e9aae6782837876262e3
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-05-09 19:47:37 +0530

    Age Calculator is ready

---
+++ Age-Calculator by js/script.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const dob = document.getElementById("DOB").value;
+    const curDate = document.getElementById("CurrentDate").value;
+
+    const Age = Number(curDate.split("-")[0] - dob.split("-")[0]);
+
+
+
+    document.getElementById("DOB").value = "";
+    document.getElementById("CurrentDate").value = "";
+    document.getElementById("Age").innerText =
+        `Your Age is : ${Age} Years`;
+
+})
