a7091d92f1bea00fc050664c674b8a33bc7782fc
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-05-24 13:11:09 +0530

    Weather App woring on Ui

---
+++ Weather-App/script.js
@@ -0,0 +1,39 @@
+// const API_URL = "5b42a433a1858d134a5b8fc6ee2bbf5b";
+// function getWeather(){
+
+// }
+
+const API_KEY = "5b42a433a1858d134a5b8fc6ee2bbf5b";
+
+async function getWeather() {
+    const cityName = document.getElementById("cityName").value.trim();
+
+    const { Lattitude, Longitude } = await getGeoLocation(cityName);
+
+    //   console.log({ Lattitude, Longitude });
+
+    const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?lat=${Lattitude}&lon=${Longitude}&appid=${API_KEY}`;
+
+    const response = await fetch(WEATHER_API);
+    const data = await response.json();
+
+    //console.log(data);
+
+    const temperature = data.main.temp - 273.15;
+
+    document.getElementById("Temperature").innerText = temperature.toFixed(2);
+}
+
+async function getGeoLocation(city) {
+    const GEO_LOC_API = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+    const response = await fetch(GEO_LOC_API);
+    const data = await response.json();
+
+    //   console.log(data);
+
+    const Lattitude = data[0].lat;
+    const Longitude = data[0].lon;
+
+    return { Lattitude, Longitude };
+}
